d5da094f1a5424027b17cc2a8dbb8a77a852e6c8
sykrik <nguyentantruc89@gmail.com>
2016-09-05 17:13:50 +0700

    + LikeAgarIO.

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+	public Camera ourCamera;
+	private Vector3 mouseLocation;
+	public float speed;
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+	//  if ourCamera is null then set the MainCamera to the variable ourCamera
+		if(ourCamera == null)
+		{
+			ourCamera = Camera.main;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseLocation = Input.mousePosition;
+		mouseLocation.z = 10;
+		direction = ourCamera.ScreenToWorldPoint(mouseLocation);
+		transform.position = Vector3.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawn : MonoBehaviour
+{
+	public GameObject _foodPrefab;
+
+	private void Start()
+	{
+		InvokeRepeating("MyFoodSpawn", 5f, 0.5f);
+	}
+
+	private void MyFoodSpawn()
+	{
+		GameObject FoodClone = Instantiate(_foodPrefab, transform.position, transform.rotation) as GameObject;
+	}
+}
